c6c1604be26fd1b05062fa8fa168bf0c5a98ef2b
JCodevanDamme <jcodevandamme@gmail.com>
2026-06-16 15:10:48 +0200

    Added Color override for Moved off Board Preview

--- game/resources/global/action/move_off_board_action.gd
+++ game/resources/global/action/move_off_board_action.gd
@@ -1,2 +1,12 @@
 extends MoveAction
 class_name MoveOffBoardAction
+
+func _init() -> void:
+	super()
+	
+	var parentPreview: Callable = previewAnimation
+	
+	previewAnimation = func(queuePos: int):
+	
+		parentPreview.call(queuePos)
+		executor.mesh.setColor(colors.actionMovedOffBoard)
